da3c254d494d4437a9a4a6580d3725354b803400
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-09-18 15:45:39 +0200

    inserisco le credenziali

--- data/db.js
+++ data/db.js
@@ -1,9 +1,16 @@
 // importo mysql
-const musql = require("mysql2")
+const mysql = require("mysql2")
 
 // Creo la connessione
-const connection = createConnection({
-  host: "",
-  user: "",
-  password:""
+const connection = mysql.createConnection({
+  host: "boolean",
+  user: "root",
+  password:"Root",
+  database: "db_blog"
+})
+
+connection.connect((err) => {
+  if(err){
+
+  }
 })
